90cf423ef5cb00f2817113c073dcc993f4458262
Avishek Chatterjee <cavishek39@gmail.com>
2021-05-12 22:39:15 +0530

    export the files in the alphabatical order

--- components/index.ts
+++ components/index.ts
@@ -1,6 +1,8 @@
-export * from './home-category/home-category.component'
-export * from './home-category/home-category.component.props'
-
 export * from './episods-list/episods-list.component'
 export * from './episods-list/episods-list.component.props'
 
+export * from './home-category/home-category.component'
+export * from './home-category/home-category.component.props'
+
+export * from './VideoPlayer/video-player.component'
+export * from './VideoPlayer/video-player.component.props'
